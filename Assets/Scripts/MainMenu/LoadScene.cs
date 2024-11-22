6765eb34e867f453f34a86deebbc4d0b14789b2c
Lechu000 <eze0317@gmail.com>
2024-11-21 21:42:05 -0300

    feedback

--- Assets/Scripts/MainMenu/LoadScene.cs
+++ Assets/Scripts/MainMenu/LoadScene.cs
@@ -1,5 +1,7 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using System.Collections;
+
 public class LoadScene : MonoBehaviour
 { 
     public void SceneLoad(string scene)
@@ -13,4 +15,33 @@ public class LoadScene : MonoBehaviour
         Application.Quit();
     }
 }
-    
+
+/*public class LoadScene : MonoBehaviour
+{
+    public void SceneLoad(string scene)
+    {
+        StartCoroutine(DelayedLoad(scene, 0.5f));
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    private IEnumerator DelayedLoad(string scene, float delay)
+    {
+        // Esperar por el tiempo especificado
+        yield return new WaitForSeconds(delay);
+
+        // Reiniciar el tiempo del juego a la velocidad normal
+        Time.timeScale = 1f;
+
+        // Cargar la nueva escena
+        SceneManager.LoadScene(scene);
+    }
+}*/
+
+//Habia echo esto para retrasar la recarga de la escena y que se escuche el sonido del boton pero rompia el volver al menu de la pausa dentro del nivel 1
+
+
+
